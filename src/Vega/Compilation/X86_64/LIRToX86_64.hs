324aefa51a62bcd037179abdfb030d4d6133d84d
prophet <mail@prophetlabs.de>
2026-01-05 13:50:03 +0100

    More compilation

--- src/Vega/Compilation/X86_64/LIRToX86_64.hs
+++ src/Vega/Compilation/X86_64/LIRToX86_64.hs
@@ -5,6 +5,7 @@ import Relude
 
 import Vega.Compilation.LIR.Syntax qualified as LIR
 import Vega.Compilation.X86_64.Syntax qualified as X86
+import Data.Traversable (for)
 
 type Compile es = () :: Constraint
 
@@ -26,4 +27,26 @@ compileDeclaration = \case
         , init
         , blocks
         } -> do
-            undefined
+            fold <$> for blocks compileBlock
+
+compileBlock :: LIR.Block -> Eff es (Seq X86.Instruction)
+compileBlock block = do
+    -- TODO: something something bb arguments
+    registerMappings <- registerAllocation block
+    instructions <- fold <$> for block.instructions (compileInstruction registerMappings)
+    terminatorInstructions <- compileTerminator block.terminator
+    pure (instructions <> terminatorInstructions)
+
+compileInstruction :: HashMap LIR.Variable RegisterMapping -> LIR.Instruction -> Eff es (Seq X86.Instruction)
+compileInstruction registerMappings = \case
+    _ -> undefined
+
+compileTerminator :: LIR.Terminator -> Eff es (Seq X86.Instruction)
+compileTerminator = undefined
+
+data RegisterMapping
+    = PhysicalRegister X86.Register
+    | SpilledToStack
+
+registerAllocation :: LIR.Block -> Eff es (HashMap LIR.Variable RegisterMapping)
+registerAllocation = pure undefined
