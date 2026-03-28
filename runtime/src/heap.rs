7cb20ef62d488f753a6c8fdcc830c65a2245b2ef
prophet <mail@prophetlabs.de>
2026-03-28 19:51:05 +0100

    Add rust runtime

---
+++ runtime/src/heap.rs
@@ -0,0 +1,66 @@
+/// The type of Vega heap objects.
+/// The fields of this type only contain the heap header
+#[repr(C)]
+pub struct HeapObject {
+    info_table: *const InfoTable,
+}
+impl HeapObject {
+    pub const HEADER_SIZE_IN_BYTES: usize = size_of::<HeapObject>();
+
+    pub fn data(object: *const HeapObject) -> *mut u8 {
+        unsafe { object.byte_offset(HeapObject::HEADER_SIZE_IN_BYTES as isize) as *mut u8 }
+    }
+}
+
+#[repr(C)]
+pub struct InfoTable {
+    object_type: ObjectType,
+    layout: Layout,
+}
+
+#[repr(C)]
+pub union Layout {
+    boxed: BoxedLayout,
+    array: ArrayLayout,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct BoxedLayout {
+    /// The full size of the object data including boxed pointers
+    size_in_bytes: usize,
+    /// The number of boxed pointers in the layout. These are always the first elements
+    boxed_count: usize
+}
+impl BoxedLayout {
+    /// The size of the unboxed part of the layout, i.e. the size of everything that is not a boxed pointer
+    pub fn unboxed_size_in_bytes(&self) -> usize {
+        self.size_in_bytes - self.boxed_count * size_of::<*const u8>()
+    }
+}
+
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct ArrayLayout {
+    size_in_elements: usize,
+    element_stride_in_bytes: usize,
+    element_boxed_count: usize
+}
+
+#[repr(C)]
+pub enum ObjectType {
+    Boxed,
+    Array,
+}
+
+// TODO: eventually we will want to inline this directly into the generated code but
+// it's simpler to keep it as a rust function for now
+// SAFETY: this assumes that info_table points to an immutable heap object
+pub unsafe fn allocate_boxed(info_table: *const InfoTable) -> *mut HeapObject {
+    let layout = unsafe { (*info_table).layout.boxed };
+
+    unsafe {
+        libc::malloc(HeapObject::HEADER_SIZE_IN_BYTES + layout.size_in_bytes) as *mut HeapObject
+    }
+}
