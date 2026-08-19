299975cf8c40644b8c1f9237661db2d3af107ea1
prophet <mail@prophetlabs.de>
2026-08-19 23:23:56 +0200

    Switch from a function pass to direct module manipulation

--- llvm-shadow-stack/src/VegaShadowStackPlugin.cpp
+++ llvm-shadow-stack/src/VegaShadowStackPlugin.cpp
@@ -126,24 +126,189 @@ struct Schedule {
     std::vector<BasicBlock *> blockOrder;
 };
 
-class ShadowStackPass : public PassInfoMixin<ShadowStackPass> {
-  public:
-    PreservedAnalyses run(Function &function, FunctionAnalysisManager &AM) {
-        auto &context = function.getContext();
-        // We should only process our own functions
-        if (function.getGC() != "vegagc") {
-            return PreservedAnalyses::all();
-        }
-        // We don't want the gc annotation to stick around since LLVM can't
-        // process it
-        // TODO: this doesn't work. we can only clear the gc annotation after we
-        // have already processed all calls to it that need to preserve it. In
-        // fact, we should probably just use a pass over the entire module,
-        // rather than one on functions
-        function.clearGC();
+unsigned idFor(PointerIDs &pointerIDs, Value *value) {
+    // TODO: surely there is a way to avoid the double traversal here
+    if (pointerIDs.pointerValueIDs.contains(value)) {
+        return pointerIDs.pointerValueIDs[value];
+    } else {
+        unsigned id = pointerIDs.pointerValueIDs.size();
+        pointerIDs.pointerValueIDs[value] = id;
+        pointerIDs.reversePointerValueIDs[id] = value;
+        return id;
+    }
+}
 
+inline bool isGCPointer(Value *value) {
+    auto type = value->getType();
+    return type->isPointerTy() && type->getPointerAddressSpace() == 1;
+}
+
+DenseMap<BasicBlock *, BitSet> computeLiveness(Function &function,
+                                               PointerIDs &pointerIDs) {
+    llvm::DenseMap<BasicBlock *, BitSet> genSets;
+    llvm::DenseMap<BasicBlock *, BitSet> killSets;
+
+    for (auto *block : post_order(&function)) {
+        BitSet genSet;
+        BitSet killSet;
+        for (auto &instruction : llvm::reverse(*block)) {
+            if (isGCPointer(&instruction)) {
+                killSet.set(idFor(pointerIDs, &instruction));
+            }
+            for (auto &operand : instruction.operands()) {
+                auto *value = operand.get();
+                if (isGCPointer(value)) {
+                    genSet.set(idFor(pointerIDs, value));
+                }
+            }
+        }
+
+        genSets[block] = genSet;
+        killSets[block] = killSet;
+    }
+
+    llvm::DenseMap<BasicBlock *, BitSet> livePointersPerBlock;
+
+    bool changed = true;
+    while (changed) {
+        changed = false;
+
+        // TODO: we might be able to do something smarter than repeatedly
+        // looping over all blocks here. This should converge pretty quickly
+        // either way though.
+        for (auto *block : post_order(&function)) {
+            BitSet liveThisIteration;
+            for (auto *successor : successors(block)) {
+                liveThisIteration |= genSets[successor];
+                liveThisIteration -= killSets[successor];
+            }
+            if (livePointersPerBlock[block] != liveThisIteration) {
+                changed = true;
+                livePointersPerBlock[block] = liveThisIteration;
+            }
+        }
+    }
+
+    return livePointersPerBlock;
+}
+
+Schedule computeSchedule(Function &function) {
+    Schedule schedule;
+    unsigned current_index = 0;
+    for (auto *block : ReversePostOrderTraversal(&function)) {
+        schedule.blockOrder.push_back(block);
+        schedule.blockStarts[block] = current_index;
+        current_index++;
+        for (auto &instruction : *block) {
+            schedule.instructions[&instruction] = current_index;
+            current_index++;
+        }
+        schedule.blockEnds[block] = current_index;
+    }
+    return schedule;
+}
+
+DenseMap<Value *, Interval>
+computeIntervals(Function &function, Schedule &schedule, PointerIDs &pointerIDs,
+                 DenseMap<BasicBlock *, BitSet> livePointersInBlock) {
+    DenseMap<Value *, Interval> intervals;
+
+    for (auto *block : reverse(schedule.blockOrder)) {
+        BitSet livePointers;
+        for (auto *successor : successors(block)) {
+            livePointers |= livePointersInBlock[successor];
+            for (auto &phi : successor->phis()) {
+                if (isGCPointer(&phi)) {
+                    livePointers.set(idFor(
+                        pointerIDs, phi.DoPHITranslation(successor, block)));
+                }
+            }
+        }
+        unsigned blockStart = schedule.blockStarts[block];
+        unsigned blockEnd = schedule.blockEnds[block];
+        for (unsigned pointerID : livePointers.set_bits()) {
+            auto *pointer = pointerIDs.reversePointerValueIDs[pointerID];
+
+            // We initially set the range of every pointer that is live
+            // after the block to the entire block. If that's not accurate,
+            // we will later refine this with Interval::setFrom.
+            intervals[pointer].addRange(blockStart, blockEnd);
+        }
+        for (auto &instruction : reverse(*block)) {
+            if (isGCPointer(&instruction)) {
+                // TODO: we could in principle avoid this hash map lookup if
+                // we computed the instruction index from the block indices
+                // here
+                intervals[&instruction].setFrom(
+                    schedule.instructions[&instruction]);
+            }
+            for (auto &operand : instruction.operands()) {
+                auto *value = operand.get();
+                if (isGCPointer(value)) {
+                    intervals[value].addRange(
+                        blockStart, schedule.instructions[&instruction]);
+                }
+            }
+        }
+    }
+
+    return intervals;
+}
+
+DenseMap<Value *, unsigned>
+allocateStackFrameSlots(DenseMap<Value *, Interval> intervals) {
+    DenseMap<Value *, unsigned> stackSlots;
+
+    BitSet usedSlots;
+
+    std::vector<std::pair<Value *, Interval>> sortedIntervals(intervals.begin(),
+                                                              intervals.end());
+    llvm::sort(sortedIntervals, [](auto &pair1, auto &pair2) {
+        return pair1.second.from < pair2.second.from;
+    });
+
+    auto increasingEndpoint = [](auto &interval1, auto &interval2) {
+        return interval1.second.to < interval2.second.to;
+    };
+    std::priority_queue<std::pair<Value *, Interval>,
+                        llvm::SmallVector<std::pair<Value *, Interval>>,
+                        decltype(increasingEndpoint)>
+        active;
+
+    for (auto &[value, interval] : sortedIntervals) {
+        while (!active.empty()) {
+            if (active.top().second.to >= interval.from) {
+                break;
+            }
+            auto [inactiveValue, inactiveInterval] = active.top();
+            active.pop();
+
+            assert(stackSlots.contains(inactiveValue));
+            usedSlots.reset(stackSlots[inactiveValue]);
+        }
+
+        unsigned slotForThisInterval = usedSlots.find_first_unset();
+        usedSlots.set(slotForThisInterval);
+        stackSlots[value] = slotForThisInterval;
+        active.push(std::make_pair(value, interval));
+    }
+
+    return stackSlots;
+}
+}; // namespace llvm
+
+extern "C" {
+void RunShadowStackPass(LLVMModuleRef moduleRef) {
+    Module *module_ = unwrap(moduleRef);
+
+    auto &context = module_->getContext();
+    for (auto &function : module_->functions()) {
+        // We should only process our own functions
+        if (!function.hasGC() || function.getGC() != "vegagc") {
+            continue;
+        }
         if (function.isDeclaration()) {
-            return PreservedAnalyses::all();
+            continue;
         }
 
         outs() << "<<<" << function.getName() << ">>>\n";
@@ -168,228 +333,19 @@ class ShadowStackPass : public PassInfoMixin<ShadowStackPass> {
             builder.CreateAlloca(PointerType::get(context, 0),
                                  ConstantInt::get(Type::getInt64Ty(context), 0),
                                  "shadow-stack-frame");
-
-        return PreservedAnalyses::none();
     }
 
-  private:
-    unsigned idFor(PointerIDs &pointerIDs, Value *value) {
-        // TODO: surely there is a way to avoid the double traversal here
-        if (pointerIDs.pointerValueIDs.contains(value)) {
-            return pointerIDs.pointerValueIDs[value];
-        } else {
-            unsigned id = pointerIDs.pointerValueIDs.size();
-            pointerIDs.pointerValueIDs[value] = id;
-            pointerIDs.reversePointerValueIDs[id] = value;
-            return id;
+    for (auto &function : module_->functions()) {
+        // We don't want the gc annotation to stick around since LLVM can't
+        // process it.
+        // However, we need to do this *after* the actual shadow stack insertion
+        // is complete, since we rely on the annotation to determine which calls
+        // need to be statepoints. Calls to other functions don't need to save
+        // boxed pointers on the shadow stack.
+        if (function.hasGC() && function.getGC() == "vegagc") {
+            function.clearGC();
         }
     }
-
-    inline bool isGCPointer(Value *value) {
-        auto type = value->getType();
-        return type->isPointerTy() && type->getPointerAddressSpace() == 1;
-    }
-
-    DenseMap<BasicBlock *, BitSet> computeLiveness(Function &function,
-                                                   PointerIDs &pointerIDs) {
-        llvm::DenseMap<BasicBlock *, BitSet> genSets;
-        llvm::DenseMap<BasicBlock *, BitSet> killSets;
-
-        for (auto *block : post_order(&function)) {
-            BitSet genSet;
-            BitSet killSet;
-            for (auto &instruction : llvm::reverse(*block)) {
-                if (isGCPointer(&instruction)) {
-                    killSet.set(idFor(pointerIDs, &instruction));
-                }
-                for (auto &operand : instruction.operands()) {
-                    auto *value = operand.get();
-                    if (isGCPointer(value)) {
-                        genSet.set(idFor(pointerIDs, value));
-                    }
-                }
-            }
-
-            genSets[block] = genSet;
-            killSets[block] = killSet;
-        }
-
-        llvm::DenseMap<BasicBlock *, BitSet> livePointersPerBlock;
-
-        bool changed = true;
-        while (changed) {
-            changed = false;
-
-            // TODO: we might be able to do something smarter than repeatedly
-            // looping over all blocks here. This should converge pretty quickly
-            // either way though.
-            for (auto *block : post_order(&function)) {
-                BitSet liveThisIteration;
-                for (auto *successor : successors(block)) {
-                    liveThisIteration |= genSets[successor];
-                    liveThisIteration -= killSets[successor];
-                }
-                if (livePointersPerBlock[block] != liveThisIteration) {
-                    changed = true;
-                    livePointersPerBlock[block] = liveThisIteration;
-                }
-            }
-        }
-
-        return livePointersPerBlock;
-    }
-
-    Schedule computeSchedule(Function &function) {
-        Schedule schedule;
-        unsigned current_index = 0;
-        for (auto *block : ReversePostOrderTraversal(&function)) {
-            schedule.blockOrder.push_back(block);
-            schedule.blockStarts[block] = current_index;
-            current_index++;
-            for (auto &instruction : *block) {
-                schedule.instructions[&instruction] = current_index;
-                current_index++;
-            }
-            schedule.blockEnds[block] = current_index;
-        }
-        return schedule;
-    }
-
-    DenseMap<Value *, Interval>
-    computeIntervals(Function &function, Schedule &schedule,
-                     PointerIDs &pointerIDs,
-                     DenseMap<BasicBlock *, BitSet> livePointersInBlock) {
-        DenseMap<Value *, Interval> intervals;
-
-        for (auto *block : reverse(schedule.blockOrder)) {
-            BitSet livePointers;
-            for (auto *successor : successors(block)) {
-                livePointers |= livePointersInBlock[successor];
-                for (auto &phi : successor->phis()) {
-                    if (isGCPointer(&phi)) {
-                        livePointers.set(
-                            idFor(pointerIDs,
-                                  phi.DoPHITranslation(successor, block)));
-                    }
-                }
-            }
-            unsigned blockStart = schedule.blockStarts[block];
-            unsigned blockEnd = schedule.blockEnds[block];
-            for (unsigned pointerID : livePointers.set_bits()) {
-                auto *pointer = pointerIDs.reversePointerValueIDs[pointerID];
-
-                // We initially set the range of every pointer that is live
-                // after the block to the entire block. If that's not accurate,
-                // we will later refine this with Interval::setFrom.
-                intervals[pointer].addRange(blockStart, blockEnd);
-            }
-            for (auto &instruction : reverse(*block)) {
-                if (isGCPointer(&instruction)) {
-                    // TODO: we could in principle avoid this hash map lookup if
-                    // we computed the instruction index from the block indices
-                    // here
-                    intervals[&instruction].setFrom(
-                        schedule.instructions[&instruction]);
-                }
-                for (auto &operand : instruction.operands()) {
-                    auto *value = operand.get();
-                    if (isGCPointer(value)) {
-                        intervals[value].addRange(
-                            blockStart, schedule.instructions[&instruction]);
-                    }
-                }
-            }
-        }
-
-        return intervals;
-    }
-
-    DenseMap<Value *, unsigned>
-    allocateStackFrameSlots(DenseMap<Value *, Interval> intervals) {
-        DenseMap<Value *, unsigned> stackSlots;
-
-        BitSet usedSlots;
-
-        std::vector<std::pair<Value *, Interval>> sortedIntervals(
-            intervals.begin(), intervals.end());
-        llvm::sort(sortedIntervals, [](auto &pair1, auto &pair2) {
-            return pair1.second.from < pair2.second.from;
-        });
-
-        auto increasingEndpoint = [](auto &interval1, auto &interval2) {
-            return interval1.second.to < interval2.second.to;
-        };
-        std::priority_queue<std::pair<Value *, Interval>,
-                            llvm::SmallVector<std::pair<Value *, Interval>>,
-                            decltype(increasingEndpoint)>
-            active;
-
-        for (auto &[value, interval] : sortedIntervals) {
-            while (!active.empty()) {
-                if (active.top().second.to >= interval.from) {
-                    break;
-                }
-                auto [inactiveValue, inactiveInterval] = active.top();
-                active.pop();
-
-                assert(stackSlots.contains(inactiveValue));
-                usedSlots.reset(stackSlots[inactiveValue]);
-            }
-
-            unsigned slotForThisInterval = usedSlots.find_first_unset();
-            usedSlots.set(slotForThisInterval);
-            stackSlots[value] = slotForThisInterval;
-            active.push(std::make_pair(value, interval));
-        }
-
-        return stackSlots;
-    }
-};
-} // namespace llvm
-
-extern "C" {
-void RunShadowStackPass(LLVMModuleRef moduleRef) {
-    Module *module_ = unwrap(moduleRef);
-
-    PassBuilder passBuilder;
-    passBuilder.registerPipelineParsingCallback(
-        [](StringRef name, FunctionPassManager &functionManager,
-           ArrayRef<PassBuilder::PipelineElement> _) {
-            if (name == "vega-shadow-stack") {
-                functionManager.addPass(ShadowStackPass());
-                return true;
-            } else {
-                return false;
-            }
-        });
-
-    LoopAnalysisManager loopAnalysisManager;
-    passBuilder.registerLoopAnalyses(loopAnalysisManager);
-    FunctionAnalysisManager functionAnalysisManager;
-    passBuilder.registerFunctionAnalyses(functionAnalysisManager);
-    CGSCCAnalysisManager cgsccAnalysisManager;
-    passBuilder.registerCGSCCAnalyses(cgsccAnalysisManager);
-    ModuleAnalysisManager moduleAnalysisManager;
-    passBuilder.registerModuleAnalyses(moduleAnalysisManager);
-
-    passBuilder.crossRegisterProxies(
-        loopAnalysisManager, functionAnalysisManager, cgsccAnalysisManager,
-        moduleAnalysisManager);
-
-    // TODO: it would be nice if we could share the analysis managers with the
-    // actual optimizations, but that would need us to restructure slightly how
-    // the compiler runs these passes.
-    ModulePassManager passManager;
-
-    if (auto error =
-            passBuilder.parsePassPipeline(passManager, "vega-shadow-stack")) {
-        errs()
-            << "internal shadow stack plugin error: unable to pass pipleine\n";
-        consumeError(std::move(error));
-        return;
-    }
-    passManager.run(*module_, moduleAnalysisManager);
 }
 }
-
 #endif // LLVM_SHADOW_STACK_PLUGIN
