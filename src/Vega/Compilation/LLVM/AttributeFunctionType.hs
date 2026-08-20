92bf4768510bd50fa2250089dc90154687d95e4b
prophet <mail@prophetlabs.de>
2026-08-20 15:57:18 +0200

    Remove frame pointer annotations
    We don't need frame pointers now that we use a shadow stack

--- src/Vega/Compilation/LLVM/AttributeFunctionType.hs
+++ src/Vega/Compilation/LLVM/AttributeFunctionType.hs
@@ -56,7 +56,6 @@ addFunctionWithAttributes :: (MonadIO io) => LLVM.Module -> Text -> AttributeFun
 addFunctionWithAttributes module_ name attributeFunctionType = do
     let functionType = rawFunctionType attributeFunctionType
     function <- LLVM.addFunction module_ name functionType
-    LLVM.addTargetDependentFunctionAttr function "frame-pointer" "all"
     applyAttributes attributeFunctionType function
     pure function
 
